fc23040d8e82ae10c1c165fd2b918da34c37d2c1
xximxxhatedxx <ghostr210305@gmail.com>
2024-12-02 13:57:37 +0100

    lab7

---
+++ WebApplication1/Controllers/FileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace WebApplication1.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        [Route("File/DownloadFile")]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("File/DownloadFile")]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                fileName = "default.txt";
+            }
+            else if (!fileName.EndsWith(".txt"))
+            {
+                fileName += ".txt";
+            }
+
+            string fileContent = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+
+            var fileBytes = Encoding.UTF8.GetBytes(fileContent);
+
+            return File(fileBytes, "text/plain", fileName);
+        }
+    }
+}
---
+++ WebApplication1/Views/FIle/DownloadFile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.FIle
+{
+    public class DownloadFileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
